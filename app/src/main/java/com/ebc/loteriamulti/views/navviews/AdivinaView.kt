fb884ab5a65e5f9cfb8eb25f03d4c37cf7eed941
Julio Caro <jcarocota@gmail.com>
2025-04-25 19:01:53 -0700

    Completamos el Adivina View

--- app/src/main/java/com/ebc/loteriamulti/views/navviews/AdivinaView.kt
+++ app/src/main/java/com/ebc/loteriamulti/views/navviews/AdivinaView.kt
@@ -6,22 +6,26 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.ebc.loteriamulti.viewmodel.AdivinaViewModel
 
 
 @Preview(showBackground = true)
 @Composable
 fun AdivinaView(
-    navController: NavHostController = rememberNavController()
+    navController: NavHostController = rememberNavController(),
+    viewModel: AdivinaViewModel = AdivinaViewModel()
 ) {
     Column(
         modifier = Modifier
@@ -39,10 +43,27 @@ fun AdivinaView(
             )
         )
 
+        viewModel.errorMessage.value?.let {
+            error ->
+                Text(text = error, color = Color.Red, modifier = Modifier.padding(top = 8.dp))
+        }
+
         Button(
-            onClick = {}
+            onClick = {viewModel.verificar()},
+            modifier = Modifier.padding(16.dp)
         ) {
             Text("¡Intentar!")
+        }
+
+        if (viewModel.isLoading.value) {
+            CircularProgressIndicator(
+                modifier = Modifier.
+                padding(top = 16.dp)
+            )
+        }
+
+        viewModel.resultado.value?.let {
+            Text(text = it, modifier = Modifier.padding(top = 16.dp))
         }
     }
 }
